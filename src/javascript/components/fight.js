ca7e711344f58bac57c2b4a60c7e5df481812dfd
Andrey Moskalenko <andrej.moskalenko@gmail.com>
2020-05-11 12:37:23 +0300

    Added Critical Hits functionality

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -4,46 +4,45 @@ import {
 
 let playerOneAttackPressed = false;
 let playerTwoAttackPressed = false;
-let playerOneBlockPressed= false;
+let playerOneBlockPressed = false;
 let playerTwoBlockPressed = false;
+let playerOneCriticalHitKeys = [false, false, false];
+let playerTwoCriticalHitKeys = [false, false, false];
+let isCriticalHitPlayerOneDisabled = false;
+let isCriticalHitPlayerTwoDisabled = false;
 
 export async function fight(firstFighter, secondFighter) {
   return new Promise((resolve) => {
     const leftHealthIndicator = document.getElementById('left-fighter-indicator');
     const rightHealthIndicator = document.getElementById('right-fighter-indicator');
-    let firstFighterHealthPercents = 100;
-    let secondFighterHealthPercents = 100;
     const leftIndicatorRate = 100 / firstFighter.health;
     const rightIndicatorRate = 100 / secondFighter.health;
+    const playerOneCriticalHitDamage = 2 * firstFighter.attack;
+    const playerTwoCriticalHitDamage = 2 * secondFighter.attack;
+
     window.addEventListener('keydown', (event) => {
       if (event.code == controls.PlayerOneAttack) {
         if (!playerOneBlockPressed && !playerTwoBlockPressed && !playerOneAttackPressed) {
           playerOneAttackPressed = true;
           let damage = getDamage(firstFighter, secondFighter);
-          secondFighter.health -= damage;
-          secondFighterHealthPercents = Math.round(secondFighter.health*rightIndicatorRate);
-          secondFighterHealthPercents = secondFighterHealthPercents>0?secondFighterHealthPercents:0;
-          rightHealthIndicator.style.width = secondFighterHealthPercents + '%';
-          if (secondFighter.health <= 0){
+          dealDamage(secondFighter, damage);
+          redrawHealthIndicator(secondFighter, rightHealthIndicator, rightIndicatorRate);
+
+          if (secondFighter.health <= 0) {
             resolve(firstFighter);
           }
-          console.log('First fighter makes damage ' + damage);
-          console.log('Second fighter health ' + secondFighter.health);
         }
       }
       if (event.code == controls.PlayerTwoAttack) {
         if (!playerOneBlockPressed && !playerTwoBlockPressed && !playerTwoAttackPressed) {
           playerTwoAttackPressed = true;
           let damage = getDamage(secondFighter, firstFighter);
-          firstFighter.health -= damage;
-          firstFighterHealthPercents = Math.round(firstFighter.health*leftIndicatorRate);
-          firstFighterHealthPercents = firstFighterHealthPercents>0?firstFighterHealthPercents:0;
-          leftHealthIndicator.style.width = firstFighterHealthPercents + '%';
-          if (firstFighter.health <= 0){
+          dealDamage(firstFighter, damage);
+          redrawHealthIndicator(firstFighter, leftHealthIndicator, leftIndicatorRate);
+
+          if (firstFighter.health <= 0) {
             resolve(secondFighter);
           }
-          console.log('Second fighter makes damage ' + damage);
-          console.log('First fighter health ' + firstFighter.health);
         }
       }
       if (event.code == controls.PlayerOneBlock) {
@@ -52,6 +51,30 @@ export async function fight(firstFighter, secondFighter) {
       if (event.code == controls.PlayerTwoBlock) {
         playerTwoBlockPressed = true;
       }
+
+      refreshCriticalHitKeysStatus(event, controls.PlayerOneCriticalHitCombination, playerOneCriticalHitKeys);
+      refreshCriticalHitKeysStatus(event, controls.PlayerTwoCriticalHitCombination, playerTwoCriticalHitKeys);
+
+      if (isCriticalHit(playerOneCriticalHitKeys) && !isCriticalHitPlayerOneDisabled) {
+        isCriticalHitPlayerOneDisabled = true;
+        dealDamage(secondFighter, playerOneCriticalHitDamage);
+        redrawHealthIndicator(secondFighter, rightHealthIndicator, rightIndicatorRate);
+        if (secondFighter.health <= 0) {
+          resolve(firstFighter);
+        }
+        setTimeout(() => (isCriticalHitPlayerOneDisabled = false), 10000);
+      }
+
+      if (isCriticalHit(playerTwoCriticalHitKeys) && !isCriticalHitPlayerTwoDisabled) {
+        isCriticalHitPlayerTwoDisabled = true;
+        dealDamage(firstFighter, playerTwoCriticalHitDamage);
+        redrawHealthIndicator(firstFighter, leftHealthIndicator, leftIndicatorRate);
+        if (firstFighter.health <= 0) {
+          resolve(secondFighter);
+        }
+        setTimeout(() => (isCriticalHitPlayerTwoDisabled = false), 10000);
+      }
+
     })
     window.addEventListener('keyup', (event) => {
       if (event.code == controls.PlayerOneAttack) {
@@ -66,8 +89,10 @@ export async function fight(firstFighter, secondFighter) {
       if (event.code == controls.PlayerTwoBlock) {
         playerTwoBlockPressed = false;
       }
-    })
 
+      refreshCriticalHitKeysStatus(event, controls.PlayerOneCriticalHitCombination, playerOneCriticalHitKeys);
+      refreshCriticalHitKeysStatus(event, controls.PlayerTwoCriticalHitCombination, playerTwoCriticalHitKeys);
+    })
   });
 }
 
@@ -91,3 +116,29 @@ export function getBlockPower(fighter) {
   const dodgeChance = Math.random() * (max - min) + min;
   return fighter.defense * dodgeChance;
 }
+
+function dealDamage(fighter, damage) {
+  fighter.health -= damage;
+}
+
+function isCriticalHit(hitKeys) {
+  return hitKeys.every(x => x) ? true : false;
+}
+
+function redrawHealthIndicator(fighter, indicator, indicatorRate) {
+  let fighterHealthPercents = Math.round(fighter.health * indicatorRate);
+  fighterHealthPercents = fighterHealthPercents > 0 ? fighterHealthPercents : 0;
+  indicator.style.width = fighterHealthPercents + '%';
+}
+
+function refreshCriticalHitKeysStatus(event, combinationCodes, combinationKeysStatus) {
+  for (let [index, item] of combinationCodes.entries()) {
+    if (event.code == item) {
+      if (combinationKeysStatus[index]) {
+        combinationKeysStatus[index] = false;
+      } else {
+        combinationKeysStatus[index] = true;
+      }
+    }
+  }
+};
